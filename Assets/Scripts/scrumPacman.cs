822feee14f10743da11307cce377eeb60f01f1dd
rgarro <rgarro@gmail.com>
2022-07-24 01:46:38 -0500

    cvs pharmacy ..

---
+++ Assets/Scripts/scrumPacman.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+ *================================================.
+ *     .-.   .-.     .--.                         |
+ *    | OO| | OO|   / _.-' .-.   .-.  .-.   .''.  |
+ *    |   | |   |   \  '-. '-'   '-'  '-'   '..'  |
+ *    '^^^' '^^^'    '--'                         |
+ *===============.  .-.  .================.  .-.  |
+ *               | |   | |                |  '-'  |
+ *               | |   | |                |       |
+ *               | ':-:' |                |  .-.  |
+ *l42            |  '-'  |                |  '-'  |
+ *==============='       '================'       |
+ *
+ *
+ *
+ *@author Rolando <rgarro@gmail.com>
+ */
+public class scrumPacman : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
