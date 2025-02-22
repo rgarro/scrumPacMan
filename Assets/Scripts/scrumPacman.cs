1f51240e088663a472dba9bf3fa0860769393392
rgarro <rgarro@gmail.com>
2025-02-22 06:10:19 -0600

    mind hacks tips and tools ..

---
+++ Assets/Scripts/scrumPacman.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+ *================================================.
+ *     .-.   .-.     .--.                         |
+ *    | OO| | OO|   / _.-' .-.   .-.  .-.   .''.  |
+ *    |   | |   |   \  '-. '-'   '-'  '-'   '..'  |
+ *    '^^^' '^^^'    '--'                         |
+ *===============.  .-.  .================.  .-.  |
+ *               | |   | |                |  '-'  |
+ *               | |   | |                |       |
+ *               | ':-:' |                |  .-.  |
+ *l42            |  '-'  |                |  '-'  |
+ *==============='       '================'       |
+ *
+ * Jonas Savimby bebe un veneno que envenena la muerte y no puede morir UNITA Unity3D
+ *
+ *@author Rolando <rgarro@gmail.com>
+ */
+public class scrumPacman : MonoBehaviour
+{
+    public bool test_flag = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# scrumPacMan
+
+Titans Throwback Nano State Machine Commander
+
+A visual helper to project your progression on comanding cleaning processes over nano state machine molecules.
+
+
+
+[![Jonas Savimby bebe un veneno que envenena la muerte y no puede morir UNITA Unity3D ... ](https://raw.githubusercontent.com/rgarro/scrumPacMan/main/preview.png)]
