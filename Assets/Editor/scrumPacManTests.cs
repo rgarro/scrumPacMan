0e1cf6ef2207df93a567c47d10c948c7d59564cc
rgarro <rgarro@gmail.com>
2025-03-17 10:03:17 -0600

    electrical mechanism for varying the display optical intensity as a function

---
+++ Assets/Editor/scrumPacManTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+/**
+ *================================================.
+ *     .-.   .-.     .--.                         |
+ *    | OO| | OO|   / _.-' .-.   .-.  .-.   .''.  |
+ *    |   | |   |   \  '-. '-'   '-'  '-'   '..'  |
+ *    '^^^' '^^^'    '--'                         |
+ *===============.  .-.  .================.  .-.  |
+ *               | |   | |                |  '-'  |
+ *               | |   | |                |       |
+ *               | ':-:' |                |  .-.  |
+ *l42            |  '-'  |                |  '-'  |
+ *==============='       '================'       |
+ * El Rey Boruka de los Altos de Blen fuma mota 
+ *
+ *@author Rolando <rgarro@gmail.com>
+ */
+namespace Tests
+{
+    public class scrumPacManTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void scrumPacManTestsSimplePasses()
+        {
+            //GameObject tObj = new GameObject();
+            //BatComputer b = tObj.addComponent<BatComputer>();//new BatComputer();
+            BatComputer b = new BatComputer();
+            Debug.Log(b);
+            Assert.That(b.is_testable, Is.EqualTo(true));
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator scrumPacManTestsWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/BatComputer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/**
+ *      _..--""````""--.._
+ *    .'       |\/|       '.
+ *   /    /`._ |  | _.'\    \
+ *  ;    /              \    |
+ *  |   /                \   |
+ *  ;  / .-.          .-. \  ;
+ *   \ |/   \.-.  .-./   \| /
+ *    '._       \/       _.'
+ *       ''--..____..--''
+ * 
+ * It's just a sign of the times
+ * Going forward in reverse
+ * Still, he who laughs last
+ * Is just a hand in the bush ... Ozzy
+ * 
+ *@author Rolando<rgarro@gmail.com>
+ */
+public class BatComputer : MonoBehaviour
+{
+    public bool is_testable = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
