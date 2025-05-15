a44ceb81bd28e1b05d7679d76dbaab7ec2e28348
rgarro <rgarro@gmail.com>
2025-05-15 10:39:14 -0600

    the second book of moses called exodus ...

--- Assets/Scripts/startBtn.cs
+++ Assets/Scripts/startBtn.cs
@@ -1,14 +1,31 @@
-﻿using System.Collections;
+﻿using System.Globalization;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class startBtn : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    
+    public bool playerStart = false;
     void Start()
     {
         Debug.Log("start button got clicked");
+        if(this.whoStarts()){
+            
+        }
     }
+
+    void gameStarter(){
+
+    }
+
+    bool whoStarts(){
+        return false;
+    }
+
 
     // Update is called once per frame
     void Update()
