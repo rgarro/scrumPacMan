c4531e891734cae1f86faeec07e942ba4df149ec
rgarro <rgarro@gmail.com>
2025-05-27 09:43:40 -0600

    mataron a gato solo con un nokia ..

--- Assets/Scripts/trellisFace.cs
+++ Assets/Scripts/trellisFace.cs
@@ -14,6 +14,14 @@ using UnityEngine;
  * The Revolution wont be Televised.
  * VOTE POR EL PADRE PARA PRESIDENTE https://www.behance.net/gallery/219138909/Padre-Minor
  *
+ * Un sociologo amigo de uniformados se queria robar el vehiculo arrendado de un cliente del bar
+ * dijo en voz alta varias veces que lo iba a robar cuando fuera pagado el arriendo
+ * el cliente del bar padecia de tener oidos de jazz y entendio la amenaza lejana y dejo de pagar el vehiculo
+ * El sociologo esta grabado con testigos que quieren vender el caso.
+ * por $5000 yo  iria con un periodista nicaraguense a fotografiar a gato solo otra vez ...
+ * los testigos queria dinero, tienen a gato solo grabado en ericcson y en nokia, y consuelan sus delitos contra la cara publica de gato solo
+ * Yo vendo ir a gritarle a gato solo el dia del homicidio en un mercado en san salvador, los campos estan en un local de la policia municipal de escazu.
+ * 
  *
  *
  *@author Rolando <rgarro@gmail.com>
@@ -30,6 +38,10 @@ public class trellisFace : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        
+    }
+
+    void clickedFaced(){
         
     }
 
