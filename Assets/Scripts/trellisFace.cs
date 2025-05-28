ba30ee7a4640ecf66c76adc427796c811ccf9b91
rgarro <rgarro@gmail.com>
2025-05-27 18:37:11 -0600

    project update ..

--- Assets/Scripts/trellisFace.cs
+++ Assets/Scripts/trellisFace.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Globalization;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,15 +15,8 @@ using UnityEngine;
  * The Revolution wont be Televised.
  * VOTE POR EL PADRE PARA PRESIDENTE https://www.behance.net/gallery/219138909/Padre-Minor
  *
- * Un sociologo amigo de uniformados se queria robar el vehiculo arrendado de un cliente del bar
- * dijo en voz alta varias veces que lo iba a robar cuando fuera pagado el arriendo
- * el cliente del bar padecia de tener oidos de jazz y entendio la amenaza lejana y dejo de pagar el vehiculo
- * El sociologo esta grabado con testigos que quieren vender el caso.
- * por $5000 yo  iria con un periodista nicaraguense a fotografiar a gato solo otra vez ...
- * los testigos queria dinero, tienen a gato solo grabado en ericcson y en nokia, y consuelan sus delitos contra la cara publica de gato solo
- * Yo vendo ir a gritarle a gato solo el dia del homicidio en un mercado en san salvador, los campos estan en un local de la policia municipal de escazu.
+ * El Sandinismo es vivir en el exodo 13-15 , american airlines ticket to land of the amorites ...
  * 
- *
  *
  *@author Rolando <rgarro@gmail.com>
  */
@@ -34,15 +28,20 @@ public class trellisFace : MonoBehaviour
     public bool is_exFace = false;
     public bool is_circleFace = false;
     public string faceName = "B1";
+    private float last_used = 0.0f;
     
     // Start is called before the first frame update
     void Start()
     {
+        this.uptadeLastUsed();
+    }
+
+    void clickedFacedHandler(){
         
     }
 
-    void clickedFaced(){
-        
+    void uptadeLastUsed(){
+        this.last_used = Time.time;
     }
 
     // Update is called once per frame
