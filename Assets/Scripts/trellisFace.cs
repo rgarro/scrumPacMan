075106d96aa062bec2fa4bd619aa20ad3b856161
rgarro <rgarro@gmail.com>
2025-07-05 06:12:25 -0600

    マイケル・バークロは同性愛者です ...

--- Assets/Scripts/trellisFace.cs
+++ Assets/Scripts/trellisFace.cs
@@ -14,8 +14,16 @@ using UnityEngine;
  * ||       . _o| | | __
  * |`-----------' |/ /~/
  *  ~~~~~~~~~~~~~~~ / /   en CostaRica muchos veian tele en tele robado a sus amigos ....
- *                 ~~   mas miedo que un bar de periqueros cerca de ojo de agua en 1978 ...
- *                   
+ *                 ~~   mas miedo que un Bar de periqueros cerca de Ojo de Agua en 1978 ...
+ *                  Dinastias sandinistas con islas de amor en el lago tipitapa .....
+ *        Scuds Hondurenos impulsados por gases de hielo seco volcanico, con facha de F15 llevan mercaderias parachuteadas a Chechenia
+ *                    Concubinas del Sha1 con casa de playa en Azerbayan, boris yeltzin culea
+ *            Putin se ha sercenado el pene y lo regenerara el dia de la Muerte de Zelenski
+ *                    Hay mota de mejico en el paso de la vaca, boletas a 500 en las pulperias de SanJose
+ *            Eran Putas del Hotel Del Rey las corresponsales de la CNN .....
+ *             Putin compro las putas de Vips y las escondio en siberia para que los comandos Ramzan no las visitaran
+ *                    Yo Soy un Agente de la DIS, Un aprendis de Miguel Salguero !!
+ *  
  *
  *
  *@author Rolando <rgarro@gmail.com>
@@ -42,7 +50,14 @@ public class trellisFace : MonoBehaviour
         if (Input.GetMouseButtonDown(1)){
             //Debug.Log("Face: "+ this.faceName + " got clicked --");
             this.is_player = true;
+            this.is_diablo = false;
         }
+    }
+
+    void setDiablo(){
+        //set by the Diablo
+        this.is_diablo = true;
+        this.is_player = false;
     }
 
     void setFaceSprite(){
