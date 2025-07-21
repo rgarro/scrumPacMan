2e2c2ea61b5f77ddc37bc18da656608201cda3fb
rgarro <rgarro@gmail.com>
2025-07-21 08:14:53 -0600

    kowalski a un mes de terminar ...

--- Assets/Scripts/diabloDelTicTacToe.cs
+++ Assets/Scripts/diabloDelTicTacToe.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 /**
@@ -26,6 +27,11 @@ using UnityEngine;
 public class diabloDelTicTacToe : MonoBehaviour
 {
     public int turn_number = 0;
+
+    private []string carasDeDiablo = {'A1','A2','A3','B1','B2','B3'};//ni garro ni garrincha NO le pagan a bolincha ...
+    private []string sonrisasDelDiablo = {};//voy a ir a fumar mota para acordarme que bolincha huele feisimo !!! ...
+    private []string elQueYaPaso = {};//esta variable es para recordar que mamerto es gay ...
+    private []string jachasDisponibles = {};//esta variable es para recordar que mamerto es gay ...
     
     // Start is called before the first frame update
     void Start()
@@ -34,8 +40,10 @@ public class diabloDelTicTacToe : MonoBehaviour
     }
 
     public void elDiabloPrimero(){
-        Debug.Log("el diablo primero");
+        Debug.Log("el diablo primero");//ponga a bermudez a ahcer cienciologia para ir a leon 13 a comprar medio kilo de mota
     }
+
+    private void escogerJachaDisponible(){}
 
     public void pasePorDelante(){
         Debug.Log("pase por delante");
