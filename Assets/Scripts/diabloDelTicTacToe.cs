9e88ff44b375a07accc8327eb91a8210333a33d8
rgarro <rgarro@gmail.com>
2025-07-21 08:33:55 -0600

    bolin bolin vaya donde yony que le van a dar 500mil ...

--- Assets/Scripts/diabloDelTicTacToe.cs
+++ Assets/Scripts/diabloDelTicTacToe.cs
@@ -28,10 +28,10 @@ public class diabloDelTicTacToe : MonoBehaviour
 {
     public int turn_number = 0;
 
-    private []string carasDeDiablo = {'A1','A2','A3','B1','B2','B3'};//ni garro ni garrincha NO le pagan a bolincha ...
-    private []string sonrisasDelDiablo = {};//voy a ir a fumar mota para acordarme que bolincha huele feisimo !!! ...
-    private []string elQueYaPaso = {};//esta variable es para recordar que mamerto es gay ...
-    private []string jachasDisponibles = {};//esta variable es para recordar que mamerto es gay ...
+    private []string carasDeDiablo = {'A1','A2','A3','B1','B2','B3','C1','C2','C3'};
+    private []string sonrisasDelDiablo = {};//bolincha va a jugar este juego dijo el diablo
+    private []string elQueYaPaso = {};
+    private []string jachasDisponibles = {};
     
     // Start is called before the first frame update
     void Start()
@@ -42,6 +42,8 @@ public class diabloDelTicTacToe : MonoBehaviour
     public void elDiabloPrimero(){
         Debug.Log("el diablo primero");//ponga a bermudez a ahcer cienciologia para ir a leon 13 a comprar medio kilo de mota
     }
+
+    private void elDiabloJuegaEsquina(){}//soplele la flauta a bolincha hasta que se ponga a pasar a alguien, luego termine el methodo
 
     private void escogerJachaDisponible(){}
 
--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 Play TicTacToe against Raptor TDD AI.
 
+2D  Xs and Os , WebGL and Android version
+
+One players who take turns against simple TDD AI  marking the spaces in a three-by-three grid, one with Xs and the other with Os. A player wins when they mark all three spaces of a row, column, or diagonal of the grid, whereupon they traditionally draw a line through those three marks to indicate the win
+
+Row2 Row2 I Found skywalker and hand solo playing with their girlfriend in the snow before the Battle of Hoth ...
+
+Buy Me a Beer, Starbucks Expresso or send a cheese burguer ... [Paypal](https://www.paypal.me/gospelOfLuke/25).
 
 
 [![herbie nichols the third world.... ](https://raw.githubusercontent.com/rgarro/scrumPacMan/main/preview.png)
