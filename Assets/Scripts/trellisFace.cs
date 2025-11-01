a22264f8cd0fb21e8cc4e75fe4e9bf5aa9a60ccc
rgarro <rgarro@gmail.com>
2025-11-01 09:08:29 -0600

    330 mA/T ..

--- Assets/Scripts/trellisFace.cs
+++ Assets/Scripts/trellisFace.cs
@@ -49,7 +49,7 @@ public class trellisFace : MonoBehaviour
     }
 
     void clickedFacedHandler(){
-         Debug.Log("ni garro ni garrincha no le pagan a bolincha, bolincha es traicionado, bernardita le robo una harina ...");
+         Debug.Log("fume mota y lea poesia ...");
         if (Input.GetMouseButtonDown(1)){
             Debug.Log("Face: "+ this.faceName + " got clicked --");
             //this.is_player = true;
