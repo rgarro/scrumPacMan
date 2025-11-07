9f85a6d2153cf5ec02691084381e6a16abb694d2
rgarro <rgarro@gmail.com>
2025-11-07 10:28:15 -0600

    project update ..

--- Assets/Scripts/restartTimeOut.cs
+++ Assets/Scripts/restartTimeOut.cs
@@ -28,8 +28,10 @@ using UnityEngine;
  *            |'-----\\--------------| |
  *            |_______^______________|,|
  *            |                      |   SSt
- * Displays cancellable TimeOut or restarts the entire game
- * supposed to be objectGetted , canceled and stopped
+ * Displays cancellable TimeOut until restarts the entire game
+ * supposed to be Relayed remotely by player action
+ *
+ * Un Delegado es un Relay Remoto, decia un perverso que compro un titulo en la U latina y se posaba de dueno ...
  *
  *
  *
@@ -48,11 +50,16 @@ public class restartTimeOut : MonoBehaviour
     
     private string timeOutStr;
     private AudioSource soundPlayer;
-    
+    public AudioClip timerSoundClip;
+
+    public bool playSoundOnTimerOn = true;
+    public float soundVolume = 0.7F;
+    public float timeOutFrom = 2.2F;//small time shot away ...
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.servoSoundPlayer = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
