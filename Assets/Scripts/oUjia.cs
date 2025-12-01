7a83ebb2ae6d05363460e2c37e95f03414962b52
rgarro <rgarro@gmail.com>
2025-12-01 09:03:37 -0600

    jotos satanicos de la ouija

--- Assets/Scripts/oUjia.cs
+++ Assets/Scripts/oUjia.cs
@@ -2,14 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 /**
-*
-* Se llama oUjia , para que sea la bujia mala y yo no tenga que reparar el alternador
-*
-*
-*@author Rolando <rgarro@gmail.com> <https://emptyart.github.io>
-*/
+ * ---------------------------
+ * |       YES      NO       |
+ * |                         |
+ * |      ABCDEFGHIJKLM      |
+ * |      NOPQRSTUVWXYZ      |
+ * |                         |
+ * |       1234567890        |
+ * |                         |
+ * |      x GOOD BYE x       |
+ * ---------------------------
+ * Se llama oUjia , para que sea la bujia mala y yo no tenga que reparar el alternador
+ * le preguntaron a la oija cuando orejas iba a culiar y salio la palabra joto
+ *
+ *
+ *
+ *@author Rolando <rgarro@gmail.com> <https://emptyart.github.io>
+ */
 public class oUjia : MonoBehaviour
 {
+    
+    private List<string> ATop = new List<string>(){"A1","B2","C3"};
+    private List<string> ABottom = new List<string>(){"A3","B2","C1"};
+    private List<string> ACenter = new List<string>(){"A2","B2","C2"};
+    private List<string> BMiddle = new List<string>(){"B1","B2","B3"};
+
     // Start is called before the first frame update
     void Start()
     {
